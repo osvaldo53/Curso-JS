bd50ce04011d9d582434448cd66d26dfc0b5263d
Osvaldo Souza <osvaldosouza53@outlook.com>
2022-02-03 15:20:49 -0300

    aula 16

---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { //se n1 nao for passado vai ser zero, se n2 nao for passado vai ser zero
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
---
+++ aula16/funcao03.js
@@ -0,0 +1,5 @@
+let v = function (x) {
+    return x*2
+}
+
+console.log(v(5)) //chamou a função atravez de uma variavel
